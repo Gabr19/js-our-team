2e41ce87321c741c1693c38a419eb2292ee043f8
Gabr19 <sannagabriele19@gmail.com>
2023-01-16 17:57:06 +0100

    inserimento foto

--- js/scripts.js
+++ js/scripts.js
@@ -7,49 +7,44 @@
 
 //  MILESTONE 2:
 // Stampare le stesse informazioni su DOM sottoforma di stringhe
+let cont = document.getElementById('container')
 
 const team = [
     {name : 'Wayne Barnett',
     role : 'Founder & CEO',
-    image : 'wayne-barnett-founde-ceo.jpg'
+    image : cont.innerHTML += '<img src="img/wayne-barnett-founder-ceo.jpg"/>'
     }, 
     {name : 'Angela Caroll',
     role : 'Chief Editor',
-    image : 'angela-caroll-chief-editor.jpg',
+    image : cont.innerHTML += '<img src="img/angela-caroll-chief-editor.jpg"/>'
     },
     {name : 'Walter Gordon',
     role : 'Office Manager',
-    image : 'walter-gordon-office-manager.jpg'
+    image : cont.innerHTML += '<img src="img/walter-gordon-office-manager.jpg"/>'
     }, 
     {name : 'Angela Lopez',
     role : 'Social Media Manager',
-    image : 'angela-lopez-social-media-manager.jpg',
+    image : cont.innerHTML +='<img src="img/angela-lopez-social-media-manager.jpg"/>'
     },
     {name : 'Scott Estrada',
     role : 'Developer',
-    image : 'scott-estrada-developer.jpg'
+    image :cont.innerHTML += '<img src="img/scott-estrada-developer.jpg"/>'
     }, 
     {name : 'Barbara Ramos',
     role : 'Graphic Designer',
-    image : 'barbara-ramos-graphic-designer.jpg',
+    image : cont.innerHTML += '<img src="img/barbara-ramos-graphic-designer.jpg"/>'
     }
 
 ];
 
-let cont = document.getElementById('container')
 
 
 for(let i = 0 ; i < team.length ; i++){
 
-
-
     for ( let key in team[i]){
          console.log(team[i][key])
-         container.innerHTML += team[i][key]
-
-       
+         container.innerHTML = team[i][key] 
      }
-
 
 }
 
